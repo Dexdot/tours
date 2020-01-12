22929bf241bab7af8b138ed1cb826b8ee8d0ed14
Dexdot <dexdotapb@gmail.com>
2020-01-12 19:29:48 +0300

    Tips, inputs

---
+++ src/blocks/tip/tip.js
@@ -0,0 +1,23 @@
+const initTip = () => {
+  const handler = (e, node) => {
+    const el = node || e.currentTarget;
+    const top = el.offsetTop + el.offsetHeight / 2;
+    const right = el.offsetLeft + el.offsetWidth;
+
+    document.documentElement.style.setProperty(
+      '--tip-btn-offset-top',
+      `${top}px`
+    );
+    document.documentElement.style.setProperty(
+      '--tip-btn-offset-left',
+      `${right}px`
+    );
+  };
+
+  $.each('.tip__btn', it => {
+    handler(null, it);
+    it.addEventListener('mouseenter', handler);
+  });
+};
+
+window.addEventListener('DOMContentLoaded', initTip);
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 import '~/js/init/css-props';
 import '~/js/init/modal';
+import '~/blocks/tip/tip';
