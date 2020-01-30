d5ac0fe99e4a32f644d6a9735cce620d5ca00fe0
Dexdot <dexdotapb@gmail.com>
2020-01-31 00:02:21 +0300

    Button pulse fix. Preloader page

--- src/blocks/preloader/preloader.js
+++ src/blocks/preloader/preloader.js
@@ -2,8 +2,6 @@ window.addEventListener('load', () => {
   const preloader = $.qs('.preloader');
   if (!preloader) return false;
 
-  setTimeout(() => {
-    preloader.style.opacity = 0;
-    preloader.style.pointerEvents = 'none';
-  }, 1000);
+  preloader.style.opacity = 0;
+  preloader.style.pointerEvents = 'none';
 });
